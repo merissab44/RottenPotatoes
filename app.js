fc698121262f14ef250bf24263644f9f8f20e3c7
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-05-04 15:54:58 -0700

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const app = express()
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('home', {msg: 'Handlebars are Cool!'})
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
