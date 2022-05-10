56c4b27f8fe24c0db2c4961cd1728b28371ceb37
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-05-10 00:56:37 -0700

    users can edit and update reviews

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('./data/db');
 const express = require('express')
+const methodOverride = require('method-override')
 const { engine } = require('express-handlebars')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -10,6 +11,7 @@ const app = express()
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // OUR MOCK ARRAY OF PROJECTS
@@ -47,7 +49,32 @@ app.get('/reviews', (req, res) => {
 })
 // NEW
 app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', {});
+  res.render('reviews-new', {title: 'New Review'});
+})
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+  Review.findById(req.params.id).lean().then((review) => {
+    res.render('reviews-edit', { review: review, title: 'Edit Review' })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${reviews._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+app.get('/reviews/:id', (req, res) => {
+  Review.find().lean().then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 const port = process.env.PORT || 3000;
