31216e0443fc1985a0c2dfa2cebf382ad3c2d711
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-05-10 22:49:27 -0700

    comment form added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('./data/db');
 const express = require('express')
+const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser');
@@ -11,6 +12,7 @@ app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + 'public'));
 
 // app.js
 
@@ -19,6 +21,7 @@ const reviews = require('./controllers/reviews')(app, Review);
 app.get('/', (req, res) => {
   res.render('reviews-index', { reviews: reviews });
 })
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('App listening on port 3000!')
